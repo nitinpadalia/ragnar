a54f762b99e157fc4ef74b0ca9fadfe0ad5ad10d
Nitin Padalia <padalia.nitin@gmail.com>
2017-02-25 14:09:20 +0530

    Initial commit

---
+++ heroes/ragnar/src/main/java/com/vikings/heroes/ragnar/RagnarApplication.java
@@ -0,0 +1,12 @@
+package com.vikings.heroes.ragnar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagnarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagnarApplication.class, args);
+	}
+}
